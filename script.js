51f4aa5b118e9513e2d9b8a25efb2e49838362f2
Josh <35703223+Juhm1@users.noreply.github.com>
2024-01-24 16:36:27 -0500

    Implement temporary modal on clicking add item button

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+var createItemModal = document.getElementById("create-item-modal");
+
 function auto_grow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight) + "px";
+}
+
+function openModal(){
+    createItemModal.style.display = "block";
+}
+
+function closeModal(){
+    createItemModal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == createItemModal){
+        closeModal();
+    }
 }
